17daa8fa2460e5f3400eb090fbacf2a1c0c97685
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-11-22 12:34:02 +0530

    .

--- certigen.m
+++ certigen.m
@@ -3,41 +3,39 @@ clear all
 close all 
 %home 
 
-filename = 'list.xlsx';
+filename = 'database.xlsx';
 [num,txt] = xlsread(filename);
-% Reads data from the first worksheet in the Microsoft Excel spreadsheet file containing details. 
-%Text is read from the file seperately from numbers
 len=length(txt);
-% obtain length of text in excel i.e the number of certificates to be generated
-blankimg = imread('jrexcom.png');
-% Read blank certificate image
+blankimg = imread('certificate_template.png');
 
-% as first row has headers, we start from second row with index 1
 for i=1:len
-    
         text_names(i,2)=txt(i,1);
-    
 end
+
 % Obtain names from the txt variable which are in 2nd column
 
 for i=1:len
-    
-      text_topic(i,3)=txt(i,2);
-    
+      text_contribution(i,3)=txt(i,2);
 end
-% Obtain team name from the txt variable which are in 3rd column
 
+% Obtain contribution from the txt variable which are in 3nd column
 
-%Ignore first row which is heading
+for i=1:len
+        text_event(i,4)=txt(i,3);
+end
+
+% Obtain event from the txt variable which are in 4th column
+
+for i=1:len
+      text_date(i,5)=txt(i,4);
+end
+
+% Obtain date from the txt variable which are in 5th column
+
 for i=2:len
-        text_all=[text_names(i,2) text_topic(i,3)];
-        % combine names and topics
+        text_all=[text_names(i,2) text_topic(i,3) text_event(i,4) text_date(i,5)];
         
-        position = [2190 1190;1405 1431];       
-        %[ x y pixel coordinates of the start of the first blank; second blank]
-        % obtain positions to insert on image, MSPaint by opening the image in
-        % it and using the pencil to point to the start pointfor the required text 
-        % and note the pixels it shows in the bottom bar
+        position = [190 830;825 990;190 1060;1125 1230];       
         
         RGB = insertText(blankimg,position,text_all,'FontSize',82,'BoxOpacity',0);
         %Default font with Font size is 22 and opacity of box is 0 means 100% transparent
